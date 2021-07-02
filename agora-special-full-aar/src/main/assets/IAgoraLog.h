64ca2ca08d404f21753b236d67937c4cbd19f751
Your Name <you@example.com>
2021-07-02 12:47:33 +0800

    3.4.5.1

--- agora-special-full-aar/src/main/assets/IAgoraLog.h
+++ agora-special-full-aar/src/main/assets/IAgoraLog.h
@@ -29,5 +29,6 @@ class ILogWriter {
   virtual int32_t writeLog(const char* message, uint16_t length) = 0;
   virtual ~ILogWriter() {}
 };
-} //namespace commons
-} //namespace agora
+
+}  // namespace commons
+}  // namespace agora
